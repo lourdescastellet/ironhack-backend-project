6f8012056a90a9f2b2c24f6cca679a0579d04e2e
Lourdes Castellet <castelletlourdes@gmail.com>
2024-07-06 17:33:40 +0200

    User update modified with unique email validator

--- src/main/java/org/ironhack/project/dtos/AdminUpdateRequest.java
+++ src/main/java/org/ironhack/project/dtos/AdminUpdateRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.ironhack.project.models.classes.UniqueEmail;
 
 @Getter
 @Setter
@@ -12,6 +13,7 @@ public class AdminUpdateRequest {
     private String name;
 
     @Email(message = "Email should be valid")
+    @UniqueEmail(message = "This email is already registered.")
     private String email;
 
     @Size(min = 5, message = "Password must be at least 5 characters long.")
